e776e7baf58a4e3f6ef33ed88ae25c59d0a566f3
emmanueljose.molina <emmanueljose.molina@salud.gob.sv>
2024-02-02 10:00:00 -0600

    Add section contact

---
+++ src/components/Contact.jsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+  return (
+    <section className="pt-40 mx-10 lg:mx-20 xl:mx-64 h-screen" id="contact">
+        <h1 className="text-colormind-text text-4xl font-bold text-center mb-10">Contacto</h1>
+    </section>
+  )
+}
