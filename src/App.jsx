75e5bc7e7a8d02521c42593dae56aa45f625e73e
emmanueljose.molina <emmanueljose.molina@salud.gob.sv>
2024-02-02 09:59:45 -0600

    Add arrows

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,29 @@ import BarraBusqueda from './components/BarraBusqueda'
 import AboutSection from "./components/AboutSection";
 import Proyects from "./components/Proyects";
 import StudiesSkills from "./components/StudiesSkills";
+import Contact from "./components/Contact";
+import { FaArrowDown, FaArrowUp  } from "react-icons/fa";
+import { Button } from "@nextui-org/react";
 
 export default function App() {
   
   const [modo, setmodo] = useState(true)
 
+  const [scroll, setScroll] = useState(window.scrollY)
+
   const chageModo = () => {
     setmodo(!modo)
+  }
+
+  window.addEventListener("scroll", function() {
+    setScroll(window.scrollY);
+  });
+
+  const bajar = () => {
+    var currentSection = document.querySelector("section:target") || document.querySelector("section:first-of-type");
+    var nextSection = currentSection.nextElementSibling || document.querySelector("section:first-of-type");
+
+    nextSection.scrollIntoView({ behavior: "smooth" });
   }
 
   return (
@@ -20,11 +36,23 @@ export default function App() {
         modo={modo}
       />
 
+      <Button className="rounded-full fixed right-0 m-8 z-20 bg-colormind-button animate-bounce" onClick={(e) => bajar(e)} isIconOnly aria-label="Bajar">
+        <FaArrowDown />
+      </Button> 
+
       <AboutSection/>
 
       <Proyects/>
 
       <StudiesSkills/>
+
+      <Contact/>
+
+      {scroll > 10 && 
+        <Button className="rounded-full fixed right-0 bottom-0 m-8 z-20 bg-colormind-button animate-bounce" isIconOnly aria-label="Bajar">
+          <FaArrowUp />
+        </Button>
+      }
 
     </main>
   );
