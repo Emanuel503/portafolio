a5516e162c5a01d02b2d1def63ad62e4add3c729
Emanuel Molina <emanueljosemolina@gmail.com>
2024-02-03 14:14:03 -0600

    Refactor app

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,27 @@
 import { useState } from "react";
-
-import BarraBusqueda from './components/BarraBusqueda'
+import BarraBusqueda from './components/NavegationBar'
 import AboutSection from "./components/AboutSection";
 import Proyects from "./components/Proyects";
 import StudiesSkills from "./components/StudiesSkills";
 import Contact from "./components/Contact";
-import { FaArrowDown, FaArrowUp  } from "react-icons/fa";
-import { Button } from "@nextui-org/react";
+import LateralContacs from "./components/LateralContacs";
+import Arrows from "./components/Arrows";
 
 export default function App() {
   
   const [modo, setmodo] = useState(true)
 
-  const [section, setSection] = useState()
-
   const chageModo = () => {
     setmodo(!modo)
   }
 
-  const bajar = (e, type) => {
-    let currentSection = document.querySelector("section:target") || document.querySelector("section:first-of-type");
-    let nextSection;
-    
-    if(type == 'down'){
-      nextSection = currentSection.nextElementSibling || document.querySelector("section:first-of-type");
-    }else{
-      nextSection = currentSection.previousElementSibling || document.querySelector("section:last-of-type")
-    }
-
-    setSection(nextSection.id)
-    window.location.href = `#${nextSection.id}`
-    nextSection.scrollIntoView({ behavior: "smooth" });
-  }
-
   return (
-    <main className={modo ? 'dark bg-colormind-background h-full': 'light bg-colormind-background h-full'}>
+    <main className={modo ? 'dark bg-colormind-background': 'light bg-colormind-background '}>
       <BarraBusqueda 
         chageModo={chageModo}
         modo={modo}
       />
       
-      {section !== 'contact' && 
-        <Button className="rounded-full fixed right-0 m-8 z-20 bg-colormind-button animate-bounce" onClick={(e) => bajar(e, 'down')} isIconOnly aria-label="Bajar">
-          <FaArrowDown />
-        </Button> 
-      }
-
       <AboutSection/>
 
       <Proyects/>
@@ -54,11 +30,9 @@ export default function App() {
 
       <Contact/>
 
-      {section !== 'about' && 
-        <Button className="rounded-full fixed right-0 bottom-0 m-8 z-20 bg-colormind-button animate-bounce" onClick={(e) => bajar(e, 'up')} isIconOnly aria-label="Bajar">
-          <FaArrowUp />
-        </Button>
-      }
+      <Arrows/>
+
+      <LateralContacs/>
 
     </main>
   );
