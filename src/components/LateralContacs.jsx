038f6d7661e038d46115ea60929e04082edc6f34
emmanueljose.molina <emmanueljose.molina@salud.gob.sv>
2024-02-05 11:21:20 -0600

    Fix redirect git hub

--- src/components/LateralContacs.jsx
+++ src/components/LateralContacs.jsx
@@ -6,7 +6,7 @@ export default function LateralContacs() {
   return (
     <div className="grid grid-cols-12 h-60 w-20 fixed z-20 top-1/2 -translate-y-1/2 -left-10 hover:left-0 transition-all" >
         <div className="col-span-7 bg-colormind-secondary rounded-r-xl flex flex-col items-center justify-center gap-3">
-            <Button title="GitHub" size="sm" isIconOnly href='https://github.com/Emanuel503' as={Link} className="bg-colormind-button text-colormind-text rounded-full text-2xl" aria-label="GitHub">
+            <Button title="GitHub" size="sm" isIconOnly href='https://github.com/Emanuel503' target="_blank" as={Link} className="bg-colormind-button text-colormind-text rounded-full text-2xl" aria-label="GitHub">
               <BiLogoGithub />
             </Button>
 
